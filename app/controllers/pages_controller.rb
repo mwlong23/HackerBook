fdcec5a4b0d4d23b0893330f9811f887dc7c9966
mitch Long <meechllada@gmail.com>
2017-12-20 15:11:35 -0800

    add comment box to homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,15 @@
 class PagesController < ApplicationController
+  before_action :set_user, except: :landing
   def landing
+    @post = Post.new
   end
-  
+
   def index
+
   end
-end
+
+  private
+  def set_user
+    @user = current_user
+  end
+  end
